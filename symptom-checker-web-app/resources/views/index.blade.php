2f7b93a2768cc974b7cf59af4fe893d6c0126dbd
FernandoQwer <skafernando2000@gmail.com>
2023-09-27 14:48:02 +0530

    DB Migration & Bootstrap 5.3.2

---
+++ symptom-checker-web-app/resources/views/index.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Test</title>
+
+        <!-- Bootstrap 5.3.2 -->
+        <link rel="stylesheet" href="{{ asset('css/bootstrap-5.3.2/bootstrap.min.css') }}">
+
+    </head>
+    <body class="bg-white">
+        <h1 class="text-primary">Hello</h1>
+
+
+
+        <!-- Boostrap 5.3.2 -->
+        <script src="{{ asset('js/bootstrap-5.3.2/bootstrap.js') }}"></script>
+    </body>
+</html>
